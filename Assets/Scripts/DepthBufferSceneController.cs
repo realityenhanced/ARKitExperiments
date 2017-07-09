b1db43be8db0989911f7d551b614bad268c9be72
Nishant <nishantkumar2004@gmail.com>
2017-07-09 16:16:49 -0700

    Updating Depth buffer every second.

--- Assets/Scripts/DepthBufferSceneController.cs
+++ Assets/Scripts/DepthBufferSceneController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class DepthBufferSceneController : SceneController {
+
+    // Constants
+    const int BytesPerDepthPixel = 2;
 
     // Script inputs
     public CursorManager m_cursorManager;
     public GameObject m_actorPrefab;
 	public Camera m_cameraToLookAt;
+    public int m_depthBufferWidth = 640;
+    public int m_depthBufferHeight = 360;
 
     // Privates
     Transform m_actorTransform;
+    byte[] m_depthBuffer;
+    Texture2D m_depthBufferTexture;
 
     void Update () {
         if (Utils.WasTouchDetected())
@@ -22,6 +29,16 @@ public class DepthBufferSceneController : SceneController {
             if (m_actorTransform == null)
             {
                 m_actorTransform = Utils.SpawnGameObjectAt(m_actorPrefab, pos, rot).transform;
+                m_depthBufferTexture = new Texture2D(m_depthBufferWidth, m_depthBufferHeight, TextureFormat.R16, false);
+
+                var renderer = m_actorTransform.GetComponent<MeshRenderer>();
+                renderer.material.mainTexture = m_depthBufferTexture;
+
+                m_depthBuffer = new byte[m_depthBufferWidth * m_depthBufferHeight * BytesPerDepthPixel];
+
+                // TMP: Start
+                StartCoroutine(TestSwitchingColors());
+                // TMP: Ends
             }
             else
             {
@@ -31,6 +48,44 @@ public class DepthBufferSceneController : SceneController {
 
 			// Face the camera
 			m_actorTransform.LookAt(m_cameraToLookAt.transform);
+        }
+    }
+
+    // Helpers
+    private void UpdateDepthBuffer()
+    {
+        if (m_depthBufferTexture)
+        {
+            m_depthBufferTexture.LoadRawTextureData(m_depthBuffer);
+        }
+    }
+
+    private IEnumerator TestSwitchingColors()
+    {
+        bool isWhite = false;
+        while (true)
+        {
+            if (isWhite)
+            {
+                SetDepthBufferColor(0.0f);
+            }
+            else
+            {
+                SetDepthBufferColor(1.0f);
+            }
+            isWhite = !isWhite;
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void SetDepthBufferColor(float val)
+    {
+        System.UInt16 uint16Val = (System.UInt16)(val * System.UInt16.MaxValue);
+        byte[] vals = BitConverter.GetBytes(uint16Val);
+        for (var i = 0; i < m_depthBuffer.GetLength(0) / BytesPerDepthPixel; ++i) {
+            m_depthBuffer.SetValue(vals[0], i*BytesPerDepthPixel);
+            m_depthBuffer.SetValue(vals[1], (i*BytesPerDepthPixel)+1);
         }
     }
 }
