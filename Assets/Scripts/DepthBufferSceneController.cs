3631ce3814cdc3ac2f1fc6d58cad088d7f3400aa
Nishant <nishantkumar2004@gmail.com>
2017-07-09 15:22:41 -0700

    Add Buffer Display prefab

--- Assets/Scripts/DepthBufferSceneController.cs
+++ Assets/Scripts/DepthBufferSceneController.cs
@@ -3,7 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DepthBufferSceneController : SceneController {
-	void Update () {
-		
-	}
+
+    // Script inputs
+    public CursorManager m_cursorManager;
+    public GameObject m_actorPrefab;
+
+    // Privates
+    Transform m_actorTransform;
+
+    void Update () {
+        if (Utils.WasTouchDetected())
+        {
+            // Place the actor & it's shadow plane at the last detected cursor postion.
+            var pos = m_cursorManager.GetCurrentCursorPosition();
+            var rot = m_cursorManager.GetCurrentCursorRotation();
+
+            if (m_actorTransform == null)
+            {
+                m_actorTransform = Utils.SpawnGameObjectAt(m_actorPrefab, pos, rot).transform;
+            }
+            else
+            {
+                m_actorTransform.position = pos;
+                m_actorTransform.rotation = rot;
+            }
+        }
+    }
 }
