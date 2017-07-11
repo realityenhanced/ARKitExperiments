8c89bdc4ba1db4d65875c3eb7aba655f37794979
Nishant Naveen <nishantkumar2004@gmail.com>
2017-07-10 23:03:08 -0700

    Render invalid hitresults as white

--- Assets/Scripts/DepthBufferSceneController.cs
+++ Assets/Scripts/DepthBufferSceneController.cs
@@ -73,13 +73,13 @@ public class DepthBufferSceneController : MonoBehaviour {
 		for (int y = 0; y < m_depthBufferHeight; ++y) {
 			for (int x = 0; x < m_depthBufferWidth; ++x) {
 				List<ARHitTestResult> hitResults = arSession.HitTest(point, ARHitTestResultType.ARHitTestResultTypeFeaturePoint);
-				if (hitResults.Count > 0) {
+				if (hitResults.Count > 0 && hitResults[0].isValid) {
                     // For now use only values between 0 and 1.
                     var d = (hitResults[0].distance < 0) ? 0 : hitResults[0].distance;
                     d = (d > 1) ? 1 : d;
                     m_depthBuffer[offset] = (byte)(d * 255);
 				} else {
-					m_depthBuffer[offset] = 0;
+					m_depthBuffer[offset] = 255;
 				}
 				offset++;
 				point.x = point.x + xdelta;
