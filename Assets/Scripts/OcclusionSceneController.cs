3827d8e9e4018642bfdedb8ceb9342f9abf9bcb3
Nishant Naveen <nishantkumar2004@gmail.com>
2017-07-16 17:19:08 -0700

    Add Occlusion Experiment scene

---
+++ Assets/Scripts/OcclusionSceneController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcclusionSceneController : ShadowSceneController {
+
+	// Script inputs
+	public GameObject m_occlusionPlanePrefab;
+
+	// Privates
+	Transform m_occlusionPlane;
+
+	// Update is called once per frame
+	void Update () {
+		if (Utils.WasTouchDetected ()) {
+			PlaceActorAtCursor ();
+			PlaceOcclusionPlaneAtCursor ();
+		}
+	}
+
+	void PlaceOcclusionPlaneAtCursor(){
+		var pos = m_cursorManager.GetCurrentCursorPosition ();
+		var rot = m_cursorManager.GetCurrentCursorRotation ();
+		if (m_occlusionPlane == null) {
+			m_occlusionPlane = Utils.SpawnGameObjectAt (m_occlusionPlanePrefab, pos, rot).transform;
+		}
+		else {
+			m_occlusionPlane.position = pos;
+			m_occlusionPlane.rotation = rot;
+		}
+	}
+}
