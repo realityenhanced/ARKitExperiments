cc891c63d5f0fc5b60177898600a0c2ee88207fa
Nishant Naveen <nishantkumar2004@gmail.com>
2017-07-19 19:48:10 -0700

    Adding hardcoded menu to launch experiments
    TODO:
    - Accept Scene names and button names in LaunchSceneConroller and create buttons dynamically
    - Make the menu responsive
    - Pressing 3 fingers together should go back to menu from any scene.

---
+++ Assets/Scripts/LauncherSceneController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LauncherSceneController : MonoBehaviour {
+
+	public void OnSceneSelection(string sceneName) {
+		SceneManager.LoadScene (sceneName, LoadSceneMode.Single);
+	}
+}
