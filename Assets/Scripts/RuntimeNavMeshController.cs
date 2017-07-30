6a2f1fdd98a6522cc137a2273307cf59f1650a61
Nishant Naveen <nishantkumar2004@gmail.com>
2017-07-29 18:33:29 -0700

    Add skeleton scene

---
+++ Assets/Scripts/RuntimeNavMeshController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeNavMeshController : SceneController {
+
+	// Use this for initialization
+	void Start () {
+		base.Start ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
