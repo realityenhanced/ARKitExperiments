dc62fb2cef016ebee188fc012ed284af16405695
Nishant <nishantkumar2004@gmail.com>
2017-08-31 21:13:32 -0700

    Adding temporary mat operations

--- NativePluginCode/opencvprocessing/operation.cpp
+++ NativePluginCode/opencvprocessing/operation.cpp
@@ -3,10 +3,44 @@ using namespace cv;
 
 extern "C"
 {
-    int PerformOperation()
+// TMP HELPERS
+#ifdef _CONSOLE 
+#define DEBUGIMG(mat) DebugDisplayImage(mat);
+#else
+#define DEBUGIMG(mat)
+#endif
+
+#ifdef _CONSOLE
+    void DebugDisplayImage(Mat& mat) {
+        namedWindow("Display window", WINDOW_NORMAL);
+        imshow("Display window", mat);
+        waitKey(0);
+    }
+#endif
+// TMP HELPERS
+
+    ////////////// JUST EXERCISING OPENCV ////////////////
+    // ASSUME imageBuffer has 3 Bytes per pixel (RGB) layout
+    int PerformOperation(uchar* imageBuffer, int numColumns, int numRows)
     {
-        // TEST
-        Mat myMat = Mat::eye(101, 21, CV_32F);
-        return myMat.cols;
+        const unsigned int NUM_CHANNELS = 3;
+        if (numColumns%NUM_CHANNELS != 0 || numRows%NUM_CHANNELS != 0)
+        {
+            printf("Invalid image size");
+            return -1;
+        }
+
+        // Create a Mat object that wraps around the passed in memory.
+        Mat input(numRows, numColumns, CV_8UC3, static_cast<void*>(imageBuffer));
+
+        DEBUGIMG(input);
+
+        // Convert the image to grayscale.
+        Mat grayscaleImage(numRows/NUM_CHANNELS, numColumns/NUM_CHANNELS, CV_8U);
+        cvtColor(input, grayscaleImage, COLOR_RGB2GRAY);
+
+        DEBUGIMG(grayscaleImage);
+
+        return 0;
     }
 }
--- NativePluginCode/opencvprocessing/windows/windowsmain.cpp
+++ NativePluginCode/opencvprocessing/windows/windowsmain.cpp
@@ -1,15 +1,38 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
-extern "C" int PerformOperation();
+extern "C" int PerformOperation(uchar* imageBuffer, int numColumns, int numRows);
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    cout << PerformOperation() << endl;
-    return 0;
+    int retVal = 0;
+    if (argc == 2)
+    {
+        char* imageName = argv[1];
+        Mat image = imread(imageName, IMREAD_COLOR);
+        if (!image.data)
+        {
+            printf("ERROR: No image data.");
+            retVal = -1;
+        }
+        else
+        {
+            namedWindow("Display window", WINDOW_NORMAL);
+            imshow("Display window", image);
+            waitKey(0);
+
+            cout << PerformOperation(image.ptr(), image.cols, image.rows) << endl;
+        }
+    }
+    else
+    {
+        printf("ERROR: Image path wasnt passed in.");
+        retVal = -1;
+    }
+
+    return retVal;
 }
 
