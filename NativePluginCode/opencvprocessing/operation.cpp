29e220339b41c1b50f18e7ef32c6e6abc9eae13e
Nishant <nishantkumar2004@gmail.com>
2017-09-05 21:29:40 -0700

    Test ORB feature detection and matching.

--- NativePluginCode/opencvprocessing/operation.cpp
+++ NativePluginCode/opencvprocessing/operation.cpp
@@ -21,8 +21,7 @@ extern "C"
 // TMP HELPERS
 
     ////////////// JUST EXERCISING OPENCV ////////////////
-    // ASSUME imageBuffer has 3 Bytes per pixel (RGB) layout
-    int PerformOperation(uchar* imageBuffer, int imageWidth, int imageHeight)
+    int Test(uchar* imageBuffer, int imageWidth, int imageHeight)
     {
         // Create a Mat object that wraps around the passed in memory.
         Mat input(imageHeight, imageWidth, CV_8UC3, static_cast<void*>(imageBuffer));
@@ -36,7 +35,7 @@ extern "C"
         // Apply a threshold & convert the image to a binary.
         threshold(input, input, 127, 150, THRESH_BINARY);
         DEBUGIMG(input);
-        
+
         // TODO: Use connectedcomponents to reduce noise.
         // ...
 
@@ -56,5 +55,85 @@ extern "C"
         DEBUGIMG(drawing);
 
         return 0;
+    }
+
+    int ImageMatcher(uchar* imageBuffer, int imageWidth, int imageHeight)
+    {
+        Mat descriptorsOfImage;
+        cv::FileStorage fsRead("input_images\\InputImage.yml", FileStorage::READ);
+        fsRead["descriptors"] >> descriptorsOfImage;
+
+        // Create a Mat object that wraps around the passed in memory.
+        Mat input(imageHeight, imageWidth, CV_8UC3, static_cast<void*>(imageBuffer));
+
+        // Convert the image to grayscale.
+        cvtColor(input, input, COLOR_RGB2GRAY);
+
+        Ptr<FeatureDetector> detector = ORB::create();
+        vector<KeyPoint> keypoints;
+        detector->detect(input, keypoints);
+
+        Ptr<DescriptorExtractor> extractor = ORB::create();
+        Mat descriptors;
+        extractor->compute(input, keypoints, descriptors);
+        descriptors.convertTo(descriptors, CV_32F);
+
+        FlannBasedMatcher matcher;
+        vector<DMatch> matches;
+        matcher.match(descriptorsOfImage, descriptors, matches);
+
+        double maxDist = 0;
+        double minDist = 100;
+        for (int i = 0; i < descriptors.rows; i++)
+        {
+            double dist = matches[i].distance;
+            if (dist < minDist) minDist = dist;
+            if (dist > maxDist) maxDist = dist;
+        }
+        cout << "Max dist : " << maxDist << endl;
+        cout << "Min dist : " << minDist << endl;
+
+        std::vector< DMatch > goodMatches;
+        for (int i = 0; i < descriptors.rows; i++)
+        {
+            if (matches[i].distance <= max(2 * minDist, 0.02))
+            {
+                goodMatches.push_back(matches[i]);
+            }
+        }
+        cout << goodMatches.size() << endl;
+        
+        return 0;
+    }
+
+    int CalcDescriptors(uchar* imageBuffer, int imageWidth, int imageHeight)
+    {
+        // Create a Mat object that wraps around the passed in memory.
+        Mat input(imageHeight, imageWidth, CV_8UC3, static_cast<void*>(imageBuffer));
+
+        // Convert the image to grayscale.
+        cvtColor(input, input, COLOR_RGB2GRAY);
+
+        Ptr<FeatureDetector> detector = ORB::create();
+
+        vector<KeyPoint> keypoints;
+        detector->detect(input, keypoints);
+
+        Ptr<DescriptorExtractor> extractor = ORB::create();
+        Mat descriptors;
+        extractor->compute(input, keypoints, descriptors);
+        descriptors.convertTo(descriptors, CV_32F);
+
+        FileStorage fsWrite("input_images\\InputImage.yml", FileStorage::WRITE);
+        fsWrite << "descriptors" << descriptors;
+        fsWrite.release();
+
+        return 0;
+    }
+
+    // ASSUME imageBuffer has 3 Bytes per pixel (RGB) layout
+    int PerformOperation(uchar* imageBuffer, int imageWidth, int imageHeight)
+    {
+        return ImageMatcher(imageBuffer, imageWidth, imageHeight);
     }
 }
