d656c4d9eaf84976e1d2ad79c74ce35f97c365d9
Nishant <nishantkumar2004@gmail.com>
2017-09-10 13:31:51 -0700

    Cleanup code

--- Assets/Plugins/OpencvProcessing/OpencvProcessingInterface.cs
+++ Assets/Plugins/OpencvProcessing/OpencvProcessingInterface.cs
@@ -2,46 +2,84 @@ using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
 
+// TODO:
+// - Figure out a way to pass the Y texture directly to the native plugin to prevent marshalling.
 public class OpencvProcessingInterface  {
-        int[] m_outArray = new int[4];
+    int[] m_outArray = new int[4];
+    IntPtr m_marshalledBuffer;
+    int m_marshalledSize;
+    byte[] m_lastBufferMarshalled;
+    int m_lastBufferMarshalledWidth;
 
-	[DllImport ("__Internal")]
-	private static extern int SaveDescriptors(IntPtr buffer, int width, int height);
+    [DllImport ("__Internal")]
+    private static extern int SaveDescriptors(IntPtr buffer, int width, int height);
 
-	[DllImport ("__Internal")]
-	private static extern int MatchDescriptors(IntPtr buffer, int width, int height, [In, Out] int[] boundingRect);
+    [DllImport ("__Internal")]
+    private static extern int MatchDescriptors(IntPtr buffer, int width, int height, [In, Out] int[] boundingRect);
 
-	public int SaveDescriptorsForFrame(Texture2D frame) {
-            // TODO: refactor and use RAII
-	    byte[] tmpBuf = frame.GetRawTextureData();
+    public int SaveDescriptorsForFrame(Texture2D frame) {
+        MarshalTexture(frame);
+        return SaveDescriptors(m_marshalledBuffer, frame.width, frame.height);
+    }
 
-	    int size = Marshal.SizeOf(tmpBuf[0]) * tmpBuf.Length;
-	    IntPtr pnt = Marshal.AllocHGlobal(size);
-			
-	    Marshal.Copy(tmpBuf, 0, pnt, tmpBuf.Length);               
+    public int MatchDescriptorsForFrame(Texture2D frame, ref Rect boundingRect) {
+        MarshalTexture(frame);
+        int val = MatchDescriptors(m_marshalledBuffer, frame.width, frame.height, m_outArray);
+        if (val == 1) {
+            boundingRect = new UnityEngine.Rect(m_outArray[0], m_outArray[1], m_outArray[2], m_outArray[3]);
+        }
 
-	    int val = SaveDescriptors(pnt, frame.width, frame.height);
-	    Marshal.FreeHGlobal(pnt);
+        return val;
+    }
 
-	    return val;
-	}
+    public Color GetColorFromFrameAt(int x, int y)
+    {
+        int pixelStart = 3 * (y * m_lastBufferMarshalledWidth + x);
 
-	public int MatchDescriptorsForFrame(Texture2D frame, ref Rect boundingRect) {
-            // TODO: refactor and use RAII
-	    byte[] tmpBuf = frame.GetRawTextureData();
+        byte r = m_lastBufferMarshalled[pixelStart];
+        byte g = m_lastBufferMarshalled[pixelStart + 1];
+        byte b = m_lastBufferMarshalled[pixelStart + 2];
 
-	    int size = Marshal.SizeOf(tmpBuf[0]) * tmpBuf.Length;
-	    IntPtr pnt = Marshal.AllocHGlobal(size);
-			
-	    Marshal.Copy(tmpBuf, 0, pnt, tmpBuf.Length);               
+        return new Color(r, g, b, 1.0f);
+    }
 
-	    int val = MatchDescriptors(pnt, frame.width, frame.height, m_outArray);
-	    Marshal.FreeHGlobal(pnt);
+    ~OpencvProcessingInterface()
+    {
+        if (m_marshalledBuffer != null)
+        {
+            Marshal.FreeHGlobal(m_marshalledBuffer);
+        }
+    }
 
-            if (val == 1) {
-                 boundingRect = new UnityEngine.Rect(m_outArray[0], m_outArray[1], m_outArray[2], m_outArray[3]);
+    private void MarshalTexture(Texture2D frame) {
+        if (frame.format != TextureFormat.RGB24)
+        {
+            Debug.LogError("Invalid texture format.");
+        }
+        else
+        {
+            if (m_lastBufferMarshalled == null || m_lastBufferMarshalled.Length != frame.width * frame.height * 3)
+            {
+                Debug.Log("Creating new buffer");
+                m_lastBufferMarshalled = frame.GetRawTextureData();
+
+                m_lastBufferMarshalledWidth = frame.width;
             }
 
-	    return val;
-	}
+            int size = Marshal.SizeOf(m_lastBufferMarshalled[0]) * m_lastBufferMarshalled.Length;
+            if (m_marshalledBuffer == null || m_marshalledSize != size)
+            {
+                if (m_marshalledBuffer != null)
+                {
+                    Debug.Log("Free old marshalled buffer");
+                    Marshal.FreeHGlobal(m_marshalledBuffer);
+                }
+                m_marshalledBuffer = Marshal.AllocHGlobal(size);
+                m_marshalledSize = size;
+
+                Debug.Log("Created new marshalled buffer");
+            }
+            Marshal.Copy(m_lastBufferMarshalled, 0, m_marshalledBuffer, m_lastBufferMarshalled.Length);
+        }
+    }
 }
--- Assets/Scripts/OpencvExperimentController.cs
+++ Assets/Scripts/OpencvExperimentController.cs
@@ -4,78 +4,88 @@ using UnityEngine;
 using UnityEngine.XR.iOS;
 
 public class OpencvExperimentController : MonoBehaviour {
+    // Inputs
+    public GameObject m_actorPrefab;
+    public float m_scanInterval = 1.0f;
+
     // Privates
     OpencvProcessingInterface m_opencvProcessing;
     bool m_isScreenBeingCaptured = false;
     CaptureFrame m_frameCapturer;
-    bool m_shouldSaveDescriptor = true;
-    public GameObject m_actorPrefab;
-    bool m_scanningStarted = false;
-
+    UnityARSessionNativeInterface m_arSession;
     private Transform m_actor;
     private Rect m_boundingRect = new Rect(0,0,0,0);
     const ARHitTestResultType m_resultTypeToUse = ARHitTestResultType.ARHitTestResultTypeFeaturePoint;
-
     ARPoint m_hitPoint = new ARPoint
     {
         x = 0.5f,
         y = 0.5f
     };
+    Material m_materialToUpdate;
+
+    enum STATE
+    {
+        UNINITIALIZED = 0,
+        WAIT_FOR_TAP,
+        START_SCANNING,
+        IN_SCANNING_MODE
+    }
+    STATE m_state = STATE.UNINITIALIZED;
 
     // Use this for initialization
     void Start () {
         m_opencvProcessing = new OpencvProcessingInterface();
         m_frameCapturer = Camera.main.GetComponent<CaptureFrame>();
+        m_arSession = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+
+        m_state = STATE.WAIT_FOR_TAP;
     }
     
     // Update is called once per frame
     void Update () {
-        if (m_shouldSaveDescriptor && m_isScreenBeingCaptured)
+        if (m_state == STATE.WAIT_FOR_TAP && m_isScreenBeingCaptured)
         {
-            Debug.Log("Feed the captured frame top opencv");
-
+            m_opencvProcessing.SaveDescriptorsForFrame(m_frameCapturer.m_lastCapturedFrame);
             m_isScreenBeingCaptured = false;
-            
-            Debug.Log(m_opencvProcessing.SaveDescriptorsForFrame(m_frameCapturer.m_lastCapturedFrame));
-
-            // Save descriptors only on the first tap.
-            m_shouldSaveDescriptor = false;
+            m_state = STATE.START_SCANNING;
         }
-        else if (m_shouldSaveDescriptor && Utils.WasTouchStartDetected())
+        else if (m_state == STATE.WAIT_FOR_TAP && Utils.WasTouchStartDetected())
         {
-            if (Utils.IsTouchOnUI())
-            {
-                // Ignore touch events if a button is pressed
-                return;
-            }
-
+            // On a touch event, capture the screen and on the next Update call, save the descriptors for the image. 
             m_frameCapturer.m_shouldCaptureOnNextFrame = true;
             m_isScreenBeingCaptured = true;
-
-            Debug.Log("Capture screen and feed the opencv function on the next frame.");
         }
-        else if (!m_shouldSaveDescriptor && !m_scanningStarted)
+        else if (m_state == STATE.START_SCANNING)
         {
-	    StartCoroutine(ScanForMatches(1.0f));
-            m_scanningStarted = true;
+            StartCoroutine(ScanForMatchesAndUpdateActorPosition(m_scanInterval));
+            m_state = STATE.IN_SCANNING_MODE;
         }
     }
 
-    private IEnumerator ScanForMatches(float timeInterval) {
+    // Coroutine that scans for matching descriptors and places the actor object at the location of the match.
+    private IEnumerator ScanForMatchesAndUpdateActorPosition(float timeInterval) {
         if (m_actor == null)
         {
             m_actor = GameObject.Instantiate(m_actorPrefab).transform;
+            m_materialToUpdate = Utils.FindMaterialOnObject(m_actor.gameObject, "COLOR BASICO 04");
         }
 
-	while(true) {
+        while(true)
+        {
             if (m_isScreenBeingCaptured)
             {
-                if (m_opencvProcessing.MatchDescriptorsForFrame(m_frameCapturer.m_lastCapturedFrame, ref m_boundingRect) == 1) {
-                    PlaceActorAt(m_boundingRect.x, m_boundingRect.y);
+                if (m_opencvProcessing.MatchDescriptorsForFrame(m_frameCapturer.m_lastCapturedFrame, ref m_boundingRect) == 1)
+                {
+                    float x = m_boundingRect.x + m_boundingRect.width / 2;
+                    float y = m_boundingRect.y + m_boundingRect.height / 2;
+
+                    PlaceActorAt(x, y);
+                    m_materialToUpdate.color = m_opencvProcessing.GetColorFromFrameAt((int)x, (int)y);
                     m_actor.gameObject.SetActive(true);
                 }
                 else
                 {
+                    // Hide the actor if no matches were found.
                     m_actor.gameObject.SetActive(false);
                 }
                 
@@ -84,7 +94,9 @@ public class OpencvExperimentController : MonoBehaviour {
             }
             else
             {
+                // Hide the actor to allow only the camera contents to be captured.
                 m_actor.gameObject.SetActive(false);
+
                 m_frameCapturer.m_shouldCaptureOnNextFrame = true;
                 m_isScreenBeingCaptured = true;
             }
@@ -96,17 +108,13 @@ public class OpencvExperimentController : MonoBehaviour {
         m_hitPoint.x = x / (float)Screen.currentResolution.width;
         m_hitPoint.y = y / (float)Screen.currentResolution.height;
 
-        Debug.Log("HIT POINT = " + m_hitPoint);
-
-        List<ARHitTestResult> hitResults =
-            UnityARSessionNativeInterface.GetARSessionNativeInterface().HitTest(m_hitPoint, m_resultTypeToUse);
+        List<ARHitTestResult> hitResults = m_arSession.HitTest(m_hitPoint, m_resultTypeToUse);
         if (hitResults.Count > 0)
         {
             ARHitTestResult hitResult = Utils.GetFirstValidHit(hitResults);
             if (hitResult.isValid)
             {
                 m_actor.position = UnityARMatrixOps.GetPosition(hitResult.worldTransform);
-                Debug.Log("Actor pos updated");
             }
         }
         else
