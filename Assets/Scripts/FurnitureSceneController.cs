8d3e231a9aafb7fdeac550c59b7f9ddb1ab2f4b1
Nishant <nishantkumar2004@gmail.com>
2017-09-23 20:00:30 -0700

    Add simple furniture placement code

--- Assets/Scripts/FurnitureSceneController.cs
+++ Assets/Scripts/FurnitureSceneController.cs
@@ -9,26 +9,50 @@ public class FurnitureSceneController : SceneController {
 	public GameObject m_actorPrefab;
 
 	// Privates
-	protected Transform m_actorTransform;
+	private Transform m_furnitureTransform;
+
+    enum FurnitureState
+    {
+        NotPlaced,
+        PlacementInProgress,
+        Placed
+    };
+    private FurnitureState m_furnitureState = FurnitureState.NotPlaced;
+
+    private Vector3 m_groundPos;
 
 	// Update is called once per frame
 	void Update() {
-		if (Utils.WasTouchStartDetected()) {
-			PlaceActorAtCursor ();
-		}
-	}
+        switch(m_furnitureState)
+        {
+            case FurnitureState.NotPlaced:
+                if (Utils.WasTouchStartDetected())
+                {
+                    // On the first tap, instantiate the furniture at the cursor location
+                    // and move the furniture along the ground (with the cursor) till the second tap is detected.
+                    m_groundPos = m_cursorManager.GetCurrentCursorPosition();
+                    m_furnitureTransform = GameObject.Instantiate(m_actorPrefab, m_groundPos, Quaternion.identity).transform;
 
-	protected void PlaceActorAtCursor ()
-	{
-		// Place the actor & it's shadow plane at the last detected cursor postion.
-		var pos = m_cursorManager.GetCurrentCursorPosition ();
-		var rot = m_cursorManager.GetCurrentCursorRotation ();
-		if (m_actorTransform == null) {
-			m_actorTransform = GameObject.Instantiate (m_actorPrefab, pos, rot).transform;
-		}
-		else {
-			m_actorTransform.position = pos;
-			m_actorTransform.rotation = rot;
-		}
+                    m_furnitureState = FurnitureState.PlacementInProgress;
+                }
+                break;
+            case FurnitureState.PlacementInProgress:
+                if (Utils.WasTouchStartDetected())
+                {
+                    // On the second tap, ground the furniture.
+                    m_furnitureState = FurnitureState.Placed;
+                }
+                else
+                {
+                    // Move the furniture along with the cursor without modifying the Y position.
+                    var cursorPos = m_cursorManager.GetCurrentCursorPosition();
+                    m_furnitureTransform.position = new Vector3(cursorPos.x, m_groundPos.y, cursorPos.z);
+                }
+                break;
+            case FurnitureState.Placed:
+                break;
+            default:
+                break;
+        }
 	}
 }
