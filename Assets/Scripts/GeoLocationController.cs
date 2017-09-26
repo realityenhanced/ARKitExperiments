ba4203f386907c3f340a8698403fdfc5618dda26
Nishant <nishantkumar2004@gmail.com>
2017-09-25 21:17:17 -0700

    Add geoloc support

--- Assets/Scripts/GeoLocationController.cs
+++ Assets/Scripts/GeoLocationController.cs
@@ -7,6 +7,7 @@ public class GeoLocationController : MonoBehaviour {
     // Script inputs
     public CursorManager m_cursorManager;
     public GameObject m_objectPrefab;
+    public Transform m_cameraTransform;
 
     // Privates
     private Transform m_objectTransform;
@@ -26,6 +27,38 @@ public class GeoLocationController : MonoBehaviour {
                 // On subsequent taps, move the object to the new location.
                 m_objectTransform.position = m_cursorManager.GetCurrentCursorPosition();
             }
+
+            StopAllCoroutines();
+            StartCoroutine(PointObjectToGeographicNorthPole(m_objectTransform, m_cameraTransform));
         }
+    }
+
+    // Helpers
+    private IEnumerator PointObjectToGeographicNorthPole(Transform objectTransform, Transform cameraTransform)
+    {
+        // On ios, the location framework prompts the user for permission.
+        Input.location.Start();
+        Input.compass.enabled = true;
+
+        // Wait until service initializes.
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.LogError("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            Debug.Log("Heading: " + Input.compass.trueHeading);
+        }
+
+        Input.location.Stop();
     }
 }
